fa96c57b2eafb2a679a3cc4ffe5be01ef61ccd6c
Amit Kumar <121509058+kumarAmit0109@users.noreply.github.com>
2026-04-04 22:56:27 +0530

    Update: islands-and-treasure - submission-4

--- Data Structures & Algorithms/islands-and-treasure/submission-4.cpp
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.cpp
@@ -1,42 +1,64 @@
 class Solution {
 public:
+    int INF = 2147483647;
+
+    int bfs(vector<vector<int>>& grid, int r, int c, vector<pair<int,int>>directions){
+        int rows = grid.size();
+        int cols = grid[0].size();
+         vector<vector<bool>>visited(rows, vector<bool>(cols, false));
+        queue<pair<int,int>>que;
+        que.push({r,c});
+        visited[r][c] = true;
+
+        int steps = 0;
+
+        while(!que.empty()){
+            int size = que.size();
+
+            for(int i = 0; i < size; i++){
+                auto [row, col] = que.front();
+                que.pop();
+
+                // if we reach treasure return steps
+                if(grid[row][col] == 0){
+                    return steps;
+                }
+
+                // traverse all four directions
+                for(auto& dir : directions){
+                    int newR = row + dir.first;
+                    int newC = col + dir.second;
+                    // if the position is within grid, and position doesnot contain water and
+                    // itnot visited then, visit it and make it entry in queue
+                    if((newR >= 0 && newR < rows) && (newC >= 0 && newC < cols) && grid[newR][newC] != -1
+                    && !visited[newR][newC]){
+                        
+                        visited[newR][newC] = true;
+                        que.push({newR, newC});
+                    }
+                }
+            }
+            // after completing a level simply increase the steps
+            steps++;
+        }
+        
+        return INF;
+        
+    }
     void islandsAndTreasure(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
 
-        queue<pair<int,int>>que;
-        // start the bfs traversal with the all the treasure point at the
-        // same time to find the nearst land.(here we reverse the problem, now
-        // we are finding the nearest land positons from each treasure point)
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] == 0){
-                    que.push({i, j});
-                }
-            }
-        }
-
         vector<pair<int,int>>directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-        while(!que.empty()){
-            int r = que.front().first;
-            int c = que.front().second;
-            que.pop();
-
-            for(auto dir : directions){
-                int newR = r + dir.first;
-                int newC = c + dir.second;
-                
-                // if the positon is outof grid or contains not land simply continue,
-                // else store the treasure dis as one more distance from the front position 
-                // distance from the treasure
-                if((newR < 0 || newR >= rows) || (newC < 0 || newC >= cols)
-                || grid[newR][newC] != INT_MAX){
-                    continue;
+       
+        // for every land position, call the bfs to find the
+        // treasure at minimum distance
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(grid[r][c] == INF){
+                    int minDis = bfs(grid, r, c, directions);
+                    grid[r][c] = minDis;
                 }
-
-                grid[newR][newC] = grid[r][c] + 1;
-                que.push({newR, newC});
             }
         }
         
