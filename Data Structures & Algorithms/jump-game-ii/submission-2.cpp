336162e287ca93057a6c1ca279585aede3478e9d
Amit Kumar <121509058+kumarAmit0109@users.noreply.github.com>
2026-04-24 22:41:47 +0530

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dfs(vector<int>&nums, int i, vector<int>& dp){
+        // if we reach end simply return zero no more jumps needed
+        if(i == nums.size() - 1){
+            return 0;
+        }
+
+        // if we reach other position than end but not able to jump further
+        // means we can move from there so return large value, indicating not able
+        // to move from that positon
+        if(nums[i] == 0){
+            return 1000000;
+        }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int result = INT_MAX;
+        int end = min((int)nums.size() - 1, i + nums[i]);
+        // try all possible jumps from current position (j = i+1 to end)
+        // for each jump destination, 1 + dfs(j) gives total jumps needed from here
+        // keep track of the minimum among all choices
+        for(int j = i+1; j <= end; j++){
+            int minJumpsReqFromCurrPostion = 1 + dfs(nums, j, dp);
+            result = min(result, minJumpsReqFromCurrPostion);
+        }
+        return dp[i] = result;
+    }
+    int jump(vector<int>& nums) {
+        // using recursion we try all possible jumps from the current index
+        // and choose the one that leads to the end using the fewest total jumps
+        // start with the 0 index
+        vector<int>dp(nums.size(), -1);
+        int ans = dfs(nums, 0, dp);
+        return ans;
+    }
+};
