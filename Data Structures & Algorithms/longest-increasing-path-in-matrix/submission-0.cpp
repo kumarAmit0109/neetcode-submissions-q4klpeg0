d24b4957df60bf13358c221787f7084c099e3f8d
Amit Kumar <121509058+kumarAmit0109@users.noreply.github.com>
2026-04-27 12:46:37 +0530

    Add: longest-increasing-path-in-matrix - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dfs(int r, int c, vector<vector<int>>& matrix, int prevVal, vector<pair<int,int>>& directions){
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        // if we reach out of bound or encounter a value which is smaller than
+        // prevVal so we cann't take that path so simply return 0
+        if((r < 0 || r >= rows) || (c < 0 || c >= cols) || (matrix[r][c] <= prevVal)){
+            return 0;
+        }
+
+        // take current and explore all the direction
+        int res = 1;
+        for(auto dir : directions){
+            int nr = r + dir.first;
+            int nc = c + dir.second;
+            res = max(res, 1 + dfs(nr, nc, matrix, matrix[r][c], directions));
+        }
+
+        return res;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int res = INT_MIN;
+        vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                int temp = dfs(r, c, matrix, INT_MIN, directions);
+                res = max(res, temp);
+
+            }
+        }
+        return res;
+    }
+};
