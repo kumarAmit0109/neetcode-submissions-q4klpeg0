4bfcb8c211e52b2c4ef773b0b83fecbdac4bb4ec
Amit Kumar <121509058+kumarAmit0109@users.noreply.github.com>
2026-04-28 20:25:02 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dfs(int indx, string s, vector<string>& wordDict){
+        // if we reach the end of string s, means s can be segmented into a space
+        // seperated sequence of dictionary words so return true;
+        if(indx == s.length()){
+            return true;
+        }
+
+        // try to find that is there any words in the dictionary either exists
+        // as a substr in string s, if yes then make a recursive call with the
+        // new index as a (initial indx + existed word.length) if from that call we 
+        // recive true simply return then
+        for(const string& word : wordDict){
+            if( indx + word.length() <= s.length() && s.substr(indx, word.length()) == word){
+                if(dfs(indx + word.length(), s, wordDict)){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // start with the 0th index for the wordDict vector
+        return dfs(0, s, wordDict);
+    }
+};
